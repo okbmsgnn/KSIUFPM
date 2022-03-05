174aee3fae9071666810664bb06c9abf37a4b0df
okabe <zeptonixx@gmail.com>
2022-03-05 23:21:53 +0200

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 import { CreateTableForm } from './modules/create-table-form';
+import fs from 'fs';
+
+const tableDirectory = '/Users/flavio/folder';
+const files = fs.readdirSync(tableDirectory);
 
 const App = () => {
+  React.useEffect(() => {
+    for (const file of files) {
+      console.log(file);
+    }
+  }, []);
+
   return (
     <App.Container>
-      asdaasdsasdasda
       <CreateTableForm />
     </App.Container>
   );
